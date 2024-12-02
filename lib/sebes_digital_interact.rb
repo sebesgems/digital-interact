e26f7772da838ed9609e37a8b76cabbd5fb78bac
Artjom Juferov <artjom.juferov@gmail.com>
2024-12-02 14:07:37 +0200

    Correct envs

--- lib/sebes_digital_interact.rb
+++ lib/sebes_digital_interact.rb
@@ -5,6 +5,4 @@ require_relative 'sebes_digital_interact/version'
 require 'sebes_digital_interact/railtie' if defined?(Rails::Railtie)
 
 module SebesDoInteract
-  class Error < StandardError; end
-  # Your code goes here...
 end
